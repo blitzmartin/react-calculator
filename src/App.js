8c7a7e4827f855bae944b4edad5fdef1d4f3c8ed
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-25 18:14:58 +0200

    operations imported and working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import * as Op from './components/Operations'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <ul>
+        <li>Addition result: {Op.add(1, 2)}</li>
+        <li>Multiplication result: {Op.multiply(2, 3)}</li>
+        <li>Subtraction result: {Op.subtract(7, 2)}</li>
+        <li>Division result: {Op.divide(5, 2)}</li>
+      </ul>
+
     </div>
   );
 }
